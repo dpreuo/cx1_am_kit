e5c52f1cc83ee5ccb0c3c0390e2034538d14b770
dpreuo <49160657+dpreuo@users.noreply.github.com>
2022-05-10 12:22:58 +0200

    added and named bash scripts

---
+++ massive_system/massive_system.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+#PBS -J 1-512
+#PBS -lselect=1:ncpus=1:mem=4gb
+#PBS -lwalltime=03:00:00
+
+module load intel-suite anaconda3/personal
+. ~/anaconda3/etc/profile.d/conda.sh
+conda activate koala
+
+python -u /rds/general/user/tch14/home/many_systems/many_systems.py
