49cf075b91d81038b592e0b7c38245923e57efab
dpreuo <49160657+dpreuo@users.noreply.github.com>
2022-05-12 15:25:19 +0200

    prepared for submission

--- massive_system/massive_system.sh
+++ massive_system/massive_system.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
-#PBS -J 1-512
-#PBS -lselect=1:ncpus=1:mem=4gb
+#PBS -J 1-16
+#PBS -lselect=1:ncpus=8:mem=4gb
 #PBS -lwalltime=03:00:00
+#PBS -e /rds/general/user/ppd19/home/kitaev_systems/massive_system/logs/errors/
+#PBS -o /rds/general/user/ppd19/home/kitaev_systems/massive_system/logs/outputs/
 
 module load intel-suite anaconda3/personal
 . ~/anaconda3/etc/profile.d/conda.sh
-conda activate koala
+conda activate base
 
-python -u /rds/general/user/tch14/home/many_systems/many_systems.py
+python -u /rds/general/user/ppd19/home/kitaev_systems/massive_system/solve_system.py
