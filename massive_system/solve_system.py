cd42779b56b0e6afd8be16ca4589e8368211b0e3
dpreuo <49160657+dpreuo@users.noreply.github.com>
2022-05-12 15:25:01 +0200

    added mpire parallel

--- massive_system/solve_system.py
+++ massive_system/solve_system.py
@@ -3,41 +3,62 @@ from koala.flux_finder import n_to_ujk_flipped
 import pickle
 import numpy as np
 import os
-job_id = int(os.environ["PBS_ARRAY_INDEX"])
+from mpire import WorkerPool
+import time
 
-job_name = 'job_' + str(job_id)
+if __name__ == '__main__':
 
-# load job info
-with open('massive_system/lattice_parameters', 'rb') as f:
-    x = pickle.load(f)
+    start_time = time.time()
+    job_id = int(os.environ["PBS_ARRAY_INDEX"]) - 1
 
-lattice = x['lattice']
-coloring = x['coloring']
-J = x['J_values']
-ujk = x['initial_ujk']
-start_and_end = x['start_and_finish_point'][job_id]
-min_spanning_set = x['spanning_tree']
-phase_resolution = x['phase_resolution']
-number_jobs_log = x['log_two_n_jobs']
+    job_name = 'job_' + str(job_id + 1 )
 
-# now look over the subset of flux sectors we have been assigned
-energies = []; gaps = []
-for val in range(start_and_end[0], start_and_end[1]):
-    new_ujk = n_to_ujk_flipped(val, ujk, min_spanning_set)
-    Hk = k_hamiltonian_generator(lattice, coloring,new_ujk,J)
-    e, g = analyse_hk(Hk, phase_resolution)
-    energies.append(e)
-    gaps.append(g)
+    # load job info
+    with open('massive_system/lattice_parameters.pickle', 'rb') as f:
+        x = pickle.load(f)
 
-gaps = np.array(gaps)
-energies = np.array(energies)
+    lattice = x['lattice']
+    coloring = x['coloring']
+    J = x['J_values']
+    ujk = x['initial_ujk']
+    start_and_end = x['start_and_finish_point'][job_id]
+    min_spanning_set = x['spanning_tree']
+    phase_resolution = x['phase_resolution']
+    number_jobs_log = x['log_two_n_jobs']
+    cores_per_batch = x['cores_per_batch']
+    prog_bar = x['prog_bar']
 
-output = {
-    'job_num': job_id, 
-    'energies': energies, 
-    'gaps': gaps,
-    'start_finish': start_and_end
-}
+    # now look over the subset of flux sectors we have been assigned
+    def find_gap_energy_from_n(index_in):
+        new_ujk = n_to_ujk_flipped(index_in, ujk, min_spanning_set)
+        Hk = k_hamiltonian_generator(lattice, coloring,new_ujk,J)
+        e, g = analyse_hk(Hk, phase_resolution)
+        return (e,g)
 
-with open('massive_system/results/'+job_name , 'wb') as f:
-    pickle.dump(output,f)
+    with WorkerPool(n_jobs=cores_per_batch) as pool:
+        results = np.array(pool.map(
+            find_gap_energy_from_n, 
+            range(start_and_end[0], start_and_end[1]),
+            progress_bar=prog_bar
+            ))
+
+    energies = []; gaps = []
+    for val in range(start_and_end[0], start_and_end[1]):
+        new_ujk = n_to_ujk_flipped(val, ujk, min_spanning_set)
+        Hk = k_hamiltonian_generator(lattice, coloring,new_ujk,J)
+        e, g = analyse_hk(Hk, phase_resolution)
+        energies.append(e)
+        gaps.append(g)
+
+    gaps = np.array(gaps)
+    energies = np.array(energies)
+
+    output = {
+        'job_num': job_id, 
+        'energies': energies, 
+        'gaps': gaps,
+        'start_finish': start_and_end
+    }
+
+    with open('massive_system/results/'+job_name , 'wb') as f:
+        pickle.dump(output,f)
