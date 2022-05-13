ce61057dcfd0ea7db09a637534a7dd5e5b4b3f40
dpreuo <49160657+dpreuo@users.noreply.github.com>
2022-05-13 23:45:19 +0200

    fixed for proper run

--- many_systems/many_systems.sh
+++ many_systems/many_systems.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-#PBS -J 1-16
-#PBS -lselect=1:ncpus=8:mem=4gb
-#PBS -lwalltime=03:00:00
+#PBS -J 1-512
+#PBS -lselect=1:ncpus=9:mem=4gb
+#PBS -lwalltime=04:00:00
 #PBS -e /rds/general/user/ppd19/home/kitaev_systems/many_systems/logs/errors/
 #PBS -o /rds/general/user/ppd19/home/kitaev_systems/many_systems/logs/outputs/
 
