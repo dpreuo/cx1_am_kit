5c134c8d5efb877a9cb0b4e5413cdf2d0f0c7bc9
dpreuo <49160657+dpreuo@users.noreply.github.com>
2022-05-13 23:46:13 +0200

    added more time for runs

--- massive_system/massive_system.sh
+++ massive_system/massive_system.sh
@@ -2,7 +2,7 @@
 
 #PBS -J 1-512
 #PBS -lselect=1:ncpus=9:mem=4gb
-#PBS -lwalltime=03:00:00
+#PBS -lwalltime=04:00:00
 #PBS -e /rds/general/user/ppd19/home/kitaev_systems/massive_system/logs/errors/
 #PBS -o /rds/general/user/ppd19/home/kitaev_systems/massive_system/logs/outputs/
 
