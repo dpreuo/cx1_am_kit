ed50ef79b7f3dbce5c6e14cc4d06ecf69aeefe58
dpreuo <49160657+dpreuo@users.noreply.github.com>
2022-05-13 23:45:33 +0200

    analyse the many results added

---
+++ many_systems/analyse_results.py
@@ -0,0 +1,58 @@
+import os
+import pickle
+from traceback import print_tb
+import numpy as np
+from koala import plotting as pl
+from koala.flux_finder import n_to_ujk_flipped, fluxes_from_bonds
+from matplotlib import pyplot as plt
+
+results_location = 'many_systems/results/'
+
+# load all the results
+full_output = []
+for filename in os.listdir(results_location):
+    with open(results_location + filename, 'rb') as f:
+        x = pickle.load(f)
+        full_output = [*full_output, *x]
+
+print(f'we have {len(full_output)} lattices to look at')
+
+def lieb_check(flux, side):
+    guess1 = -(1j)**side
+    guess2 = -(-1j)**side
+
+    guess1 = (guess1.real + guess1.imag).astype('int') 
+    guess2 = (guess2.real + guess2.imag).astype('int') 
+
+    out = False
+    if flux == guess1 or flux == guess2:
+        out = True
+
+    return out
+
+
+for n,result in enumerate(full_output):
+    # load lattice
+    lattice = result['lattice']
+    energies = result['energies']
+    gaps = result['gaps']
+    spanning_tree = result['spanning_tree']
+    
+    # find the winning flux etc
+    ujk = np.ones(lattice.n_edges)
+    winner = np.argmin(energies)
+    winning_ujk = n_to_ujk_flipped(winner, ujk, spanning_tree)
+    fluxes = fluxes_from_bonds(lattice, winning_ujk)
+    all_sides = np.array([plaq.n_sides for plaq in lattice.plaquettes])
+
+    # check lieb result
+    for f, s in zip(fluxes, all_sides):
+        if not lieb_check(f,s):
+            print(f'index {n:04d} --- Fails Lieb check!!!!!')
+    
+    # check gaps
+    g = gaps[winner]
+    if g<1e-10:
+        print(f'index {n:04d} --- gap: {g:.2E}')
+
+    
