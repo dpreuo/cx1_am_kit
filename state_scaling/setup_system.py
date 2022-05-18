96856cc6b69e22567c247125964f093d56219b6a
dpreuo <49160657+dpreuo@users.noreply.github.com>
2022-05-18 15:50:21 +0200

    removed mpire and better memory management

--- state_scaling/setup_system.py
+++ state_scaling/setup_system.py
@@ -19,43 +19,43 @@ def scale_points_down(points, scaling:float):
 
 if __name__ == '__main__':
 
+    # run at home
+    location = '/Users/perudornellas/python/imperial/cx1_am_kit/state_scaling/'
     max_system_size = 30
+
+    # run on cx1
+    # max_system_size = 50
+    # location = '/rds/general/user/ppd19/home/cx1_am_kit/state_scaling/'
+
+ 
     N = max_system_size**2
+    number_of_scales = 500
 
     points_max = generate_random(N)
     j_vals = np.array([1,1,1])
 
-    flux_sectors_per_job = 1
-
-    scales = np.linspace(0.2,1,500)
+    scales = np.linspace(0.2,1,number_of_scales)
     
-    location = '/rds/general/user/ppd19/home/cx1_am_kit/state_scaling/'
-
-    # generate all the systems
-    all_systems = []
-    for s in tqdm(scales):
-        scaled_points = scale_points_down(points_max, s)
-        lattice = generate_lattice(scaled_points)
-        coloring = color_lattice(lattice)
-        all_sides = np.array([p.n_sides for p in lattice.plaquettes])
-        gnd_flux_sector = -(1j)**all_sides; gnd_flux_sector = gnd_flux_sector.real + gnd_flux_sector.imag
-        gnd_ujk =find_flux_sector(lattice, gnd_flux_sector)
-
-        res_dict = {
-            'lattice': lattice,
-            'coloring': coloring,
-            'ground_state_ujk': gnd_ujk,
-            'scale': s
-        }
-    
-        all_systems.append(res_dict)
-
-    output = {
+    metaparameters = {
         'max_points': points_max,
-        'flux_sectors_per_job': flux_sectors_per_job,
         'j_vals': j_vals,
-        'all_systems': all_systems
+        'scales': scales
     }
-
     with open(location + 'systems.pickle', 'wb') as f:
-        pkl.dump(output, f)
+        pkl.dump(metaparameters, f)
+
+        # generate all the systems
+        for s in tqdm(scales):
+            scaled_points = scale_points_down(points_max, s)
+            lattice = generate_lattice(scaled_points)
+            coloring = color_lattice(lattice)
+            all_sides = np.array([p.n_sides for p in lattice.plaquettes])
+            gnd_flux_sector = -(1j)**all_sides; gnd_flux_sector = gnd_flux_sector.real + gnd_flux_sector.imag
+            gnd_ujk =find_flux_sector(lattice, gnd_flux_sector)
+
+            res_dict = (lattice, coloring, gnd_ujk, s)
+        
+            pkl.dump(res_dict, f)
+
+    
+
--- state_scaling/solve_systems.py
+++ state_scaling/solve_systems.py
@@ -9,37 +9,46 @@ from mpire import WorkerPool
 import pickle as pkl
 import os
 from mpire import WorkerPool
+from tqdm import tqdm
+
 
 if __name__ == '__main__':
 
-    job_id = int(os.environ["PBS_ARRAY_INDEX"]) 
-    input_location = '/rds/general/user/ppd19/home/cx1_am_kit/state_scaling/systems.pickle'
-    results_location = '/rds/general/user/ppd19/home/cx1_am_kit/state_scaling/results/'
+    # run at home
+    job_id = 3
+    input_location = '/Users/perudornellas/python/imperial/cx1_am_kit/state_scaling/systems.pickle'
+    results_location = '/Users/perudornellas/python/imperial/cx1_am_kit/state_scaling/results/'
 
-    with open(input_location, 'rb') as f:
-        x = pkl.load(f)
-        max_points = x['max_points']
-        flux_sectors_per_job = x['flux_sectors_per_job']
-        j_vals = x['j_vals']
-        all_systems = x['all_systems']
-        del x
+    # run on cx1
+    # job_id = int(os.environ["PBS_ARRAY_INDEX"]) 
+    # input_location = '/rds/general/user/ppd19/home/cx1_am_kit/state_scaling/systems.pickle'
+    # results_location = '/rds/general/user/ppd19/home/cx1_am_kit/state_scaling/results/'
 
-    def analyse_system(n):
-        system = all_systems[n]
-        l = system['lattice']
-        coloring = system['coloring']
-        ujk = 1 - 2*np.random.randint(0,2,l.n_edges)
-        hamiltonian = generate_majorana_hamiltonian(l, coloring, ujk, j_vals)
+    with open(input_location, 'rb') as f_in:
 
-        return np.linalg.eigh(hamiltonian), ujk
+        # load the metaparameters
+        metaparmeters = pkl.load(f_in)
+        points_max = metaparmeters['max_points'] 
+        j_vals = metaparmeters['j_vals'] 
+        scales = metaparmeters['scales']
+        number_of_scales = len(scales)  
 
-    all_solved_systems = []
-    for round in range(flux_sectors_per_job):
-        with WorkerPool(n_jobs=8) as pool:
-            solved_systems = pool.map(analyse_system, range(len(all_systems)), progress_bar=True)
-        all_solved_systems.append(solved_systems)
-    
-    with open(results_location + f'job_{job_id}.pickle', 'wb') as f:
-        pkl.dump(all_solved_systems)
+        with open(results_location + f'job_{job_id}.pickle', 'wb') as f:
+
+            # for each scale, solve the hamiltonian and dump the result
+            for n in tqdm(range(number_of_scales)):
+
+                lattice, coloring, gnd_ujk, s = pkl.load(f_in)
+                ujk = 1 - 2*np.random.randint(0,2,lattice.n_edges)
+                hamiltonian = generate_majorana_hamiltonian(lattice, coloring, ujk, j_vals)
+                e,v = np.linalg.eigh(hamiltonian)
+                output = {
+                    'system_index': n,
+                    'energies': e,
+                    'eigenstates': v,
+                    'ujk': ujk
+                }
+                pkl.dump(output,f)
+
 
     
