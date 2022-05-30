8b78500f891f3e379b4edee8c181689044989305
dpreuo <49160657+dpreuo@users.noreply.github.com>
2022-05-30 10:32:36 +0200

    add run file

---
+++ multiscale_tests/solve_multiscale.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+#PBS -J 1-10
+#PBS -lselect=1:ncpus=1:mem=4gb
+#PBS -lwalltime=04:00:00
+#PBS -e /rds/general/user/ppd19/home/cx1_am_kit/multiscale_testslogs/errors/
+#PBS -o /rds/general/user/ppd19/home/cx1_am_kit/multiscale_testslogs/outputs/
+
+module load intel-suite anaconda3/personal
+. ~/anaconda3/etc/profile.d/conda.sh
+conda activate base
+
+python -u /rds/general/user/ppd19/home/cx1_am_kit/multiscale_tests/solve_system.py
