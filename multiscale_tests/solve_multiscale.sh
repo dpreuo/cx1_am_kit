5d8a634cbb2791ccef43fe11f293c84dacda0bc8
dpreuo <49160657+dpreuo@users.noreply.github.com>
2022-05-30 10:39:51 +0200

    typo

--- multiscale_tests/solve_multiscale.sh
+++ multiscale_tests/solve_multiscale.sh
@@ -3,8 +3,8 @@
 #PBS -J 1-10
 #PBS -lselect=1:ncpus=1:mem=4gb
 #PBS -lwalltime=04:00:00
-#PBS -e /rds/general/user/ppd19/home/cx1_am_kit/multiscale_testslogs/errors/
-#PBS -o /rds/general/user/ppd19/home/cx1_am_kit/multiscale_testslogs/outputs/
+#PBS -e /rds/general/user/ppd19/home/cx1_am_kit/multiscale_tests/logs/errors/
+#PBS -o /rds/general/user/ppd19/home/cx1_am_kit/multiscale_tests/logs/outputs/
 
 module load intel-suite anaconda3/personal
 . ~/anaconda3/etc/profile.d/conda.sh
