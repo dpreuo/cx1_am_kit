dd8b0090717ada98dec55da713d7dff5bf4beb3d
dpreuo <49160657+dpreuo@users.noreply.github.com>
2022-06-14 18:42:57 +0200

    edited for new file structure

--- many_systems/many_systems.sh
+++ many_systems/many_systems.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-#PBS -J 1-512
+#PBS -J 1-4096
 #PBS -lselect=1:ncpus=9:mem=4gb
-#PBS -lwalltime=04:00:00
-#PBS -e /rds/general/user/ppd19/home/kitaev_systems/many_systems/logs/errors/
-#PBS -o /rds/general/user/ppd19/home/kitaev_systems/many_systems/logs/outputs/
+#PBS -lwalltime=06:00:00
+#PBS -e /rds/general/user/ppd19/home/cx1_am_kit/many_systems/logs/errors/
+#PBS -o /rds/general/user/ppd19/home/cx1_am_kit/many_systems/many_systems/logs/outputs/
 
 module load intel-suite anaconda3/personal
 . ~/anaconda3/etc/profile.d/conda.sh
 conda activate base
 
-python -u /rds/general/user/ppd19/home/kitaev_systems/many_systems/many_systems.py
+python -u /rds/general/user/ppd19/home/cx1_am_kit/many_systems/many_systems.py
