848a55bf0953dc0ccd6deabd19b81cb165fe9804
dpreuo <49160657+dpreuo@users.noreply.github.com>
2022-06-23 17:18:05 +0200

    bug fix

--- many_systems/many_systems.sh
+++ many_systems/many_systems.sh
@@ -4,7 +4,7 @@
 #PBS -lselect=1:ncpus=9:mem=4gb
 #PBS -lwalltime=06:00:00
 #PBS -e /rds/general/user/ppd19/home/cx1_am_kit/many_systems/logs/errors/
-#PBS -o /rds/general/user/ppd19/home/cx1_am_kit/many_systems/many_systems/logs/outputs/
+#PBS -o /rds/general/user/ppd19/home/cx1_am_kit/many_systems/logs/outputs/
 
 module load intel-suite anaconda3/personal
 . ~/anaconda3/etc/profile.d/conda.sh
--- many_systems_random_j/random_j_solve.sh
+++ many_systems_random_j/random_j_solve.sh
@@ -3,8 +3,8 @@
 #PBS -J 1-4096
 #PBS -lselect=1:ncpus=9:mem=4gb
 #PBS -lwalltime=06:00:00
-#PBS -e /rds/general/user/ppd19/home/cx1_am_kit/many_systems/logs/errors/
-#PBS -o /rds/general/user/ppd19/home/cx1_am_kit/many_systems/many_systems/logs/outputs/
+#PBS -e /rds/general/user/ppd19/home/cx1_am_kit/many_systems_random_j/logs/errors/
+#PBS -o /rds/general/user/ppd19/home/cx1_am_kit/many_systems_random_j/logs/outputs/
 
 module load intel-suite anaconda3/personal
 . ~/anaconda3/etc/profile.d/conda.sh
